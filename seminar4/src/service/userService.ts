1e296e6190960c220e66bbb5d48009939cd5fc87
gahee99 <u1879@sookmyung.ac.kr>
2022-11-11 20:42:45 +0900

    4차 세미나 과제 제출

--- seminar4/src/service/userService.ts
+++ seminar4/src/service/userService.ts
@@ -1,5 +1,45 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+//* 유저 생성
+const createUser = async (username: string, email: string, age: number) => {
+  const data = await prisma.user.create({
+    data: {
+      username,
+      email,
+      age,
+    },
+  });
+
+  return data;
+};
+
+//* 유저 전체 조회
+const getAllUser = async () => {
+  const data = await prisma.user.findMany();
+  return data;
+};
+
+//* 유저 정보 수정
+const updateUser = async (userId: number, name: string) => {
+  const data = await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      username: name
+    }
+  })
+  return data;
+};
+
+//* 유저 삭제
+const deleteUser = async (userId: number) => {
+  await prisma.user.delete({
+    where: {
+      id: userId
+    }
+  })
+};
 
 //* userId로 유저 조회
 const getUserById = async (userId: number) => {
@@ -13,6 +53,10 @@ const getUserById = async (userId: number) => {
 };
 
 const userService = {
+  createUser,
+  getAllUser,
+  updateUser,
+  deleteUser,
   getUserById,
 };
 
